560cfd27e5bf1d93d428c598ee2fee4d24f65fd2
Jungma <mbjun3630@naver.com>
2023-01-26 17:29:59 +0900

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
-import './globals.css'
+import React from 'react'
 
-export default function RootLayout({
-  children,
-}: {
+interface Props {
   children: React.ReactNode
-}) {
+}
+
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html lang="ko">
       <body>{children}</body>
     </html>
   )
